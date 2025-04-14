93a8474a8b8f4263d96cd1ed760b6e743e1b7878
Amgad Abdullah <amga200d@gmail.com>
2025-04-14 01:12:20 -0600

    Adding the TopNavigationBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 // import PhotoListItem from './components/PhotoListItem';
 // import PhotoList from './components/PhotoList';
 // import TopicListItem from './components/TopicListItem';
-import TopicList from './components/TopicList';
+// import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <TopicList/>
+      <TopNavigationBar />
     </div>
   );
 };
