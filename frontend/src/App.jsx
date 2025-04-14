9a0348d95554bf152ed7c2b3c3b1e3a67d11092c
Amgad Abdullah <amga200d@gmail.com>
2025-04-13 20:02:42 -0600

    refactoring and adding props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
+const sampleDataForPhotoListItem = {
+  id: 1,
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  urls: {
+    full: "/Image-1-Full.jpeg",
+    regular: "/Image-1-Regular.jpeg",
+  },
+  user: {
+    username: "exampleuser",
+    name: "Joe Example",
+    profile: "/profile-1.jpg",
+  },
+};
+
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem
+        id={sampleDataForPhotoListItem.id}
+        imageSource={sampleDataForPhotoListItem.urls.regular}
+        username={sampleDataForPhotoListItem.user.username}
+        location={`${sampleDataForPhotoListItem.location.city}, ${sampleDataForPhotoListItem.location.country}`}
+        profile={sampleDataForPhotoListItem.user.profile}
+      />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,42 +1,17 @@
+import React from "react";
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
-
-const PhotoListItem = () => {
-  const {
-    id,
-    location,
-    urls,
-    user
-  } = sampleDataForPhotoListItem;
-
-  const locationString = `${location.city}, ${location.country}`;
-
+const PhotoListItem = ({ id, imageSource, username, location, profile }) => {
   return (
     <div className="photo-list-item" key={id}>
-      <img className="photo-image" src={urls.regular} alt={`Photo by ${user.name}`} />
+      <img className="photo-image" src={imageSource} alt={`Photo by ${username}`} />
       <div className="photo-details">
         <div className="user-info">
-          <img className="user-profile" src={user.profile} alt={`${user.name}'s profile`} />
-          <span className="username">@{user.username}</span>
+          <img className="user-profile" src={profile} alt={`${username}'s profile`} />
+          <span className="username">@{username}</span>
         </div>
         <div className="location">
-          {locationString}
+          {location}
         </div>
       </div>
     </div>
