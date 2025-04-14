1bc2a30c6b079ed9341d18ae3b699aab52defdef
Amgad Abdullah <amga200d@gmail.com>
2025-04-14 01:45:42 -0600

    refactor to using mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import React from 'react';
 // import TopicList from './components/TopicList';
 // import TopNavigationBar from './components/TopNavigationBar';
 import HomeRoute from "./components/HomeRoute";
+import photos from "./mocks/photos";
+import topics from "./mocks/topics";
 
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+       <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "../styles/HomeRoute.scss";
-
 import TopNavigation from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics }) => {
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };
